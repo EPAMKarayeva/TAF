d3047369c1ca1fdbacd8cd5b44227a174d1d274e
Aziza Karayeva <Aziza_Karayeva@epam.com>
2024-12-02 16:09:50 +0500

    Removed unnecessary links

--- TAF/Business/Page Objects/BasePage.cs
+++ TAF/Business/Page Objects/BasePage.cs
@@ -1,13 +1,6 @@
 ﻿using NLog;
 using OpenQA.Selenium;
-using OpenQA.Selenium.BiDi.Modules.BrowsingContext;
 using OpenQA.Selenium.Support.UI;
-using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TAF.Business.Page_Objects
 {
